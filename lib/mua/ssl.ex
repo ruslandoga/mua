cb55157ec261ea0fb5fd5fba01fe73ca2b90684e
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-07-10 02:56:01 +0700

    format

--- lib/mua/ssl.ex
+++ lib/mua/ssl.ex
@@ -362,7 +362,7 @@ defmodule Mua.SSL do
   # therefore the only pattern used in commercially issued certificates.
   defp match_fun({:dns_id, reference}, {:dNSName, [?*, ?. | presented]}) do
     case domain_without_host(reference) do
-      '' -> :default
+      ~c"" -> :default
       domain -> :string.casefold(domain) == :string.casefold(presented)
     end
   end
