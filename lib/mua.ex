bdbc11084438176ce830a927cac5b2265b444162
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-11-08 22:03:44 +0700

    :binary.replace/4 goes brrr

--- lib/mua.ex
+++ lib/mua.ex
@@ -418,39 +418,15 @@ defmodule Mua do
   @spec data(socket, iodata, timeout) :: {:ok, receipt :: String.t()} | error
   def data(socket, message, timeout \\ @default_timeout) do
     # TODO implement proper
-    message = escape_leading_dot(message)
+    message = :binary.replace(message, "\n.", "\n..")
 
     with {:ok, [354 | _lines]} <- request(socket, "DATA\r\n", timeout),
-         {:ok, [250 | lines]} <- request(socket, [message | "\r\n.\r\n"], timeout) do
+         {:ok, [250 | lines]} <- request(socket, [message | ".\r\n"], timeout) do
       {:ok, IO.iodata_to_binary(lines)}
     else
       {:ok, [code | lines]} -> smtp_error(code, lines)
       {:error, _reason} = error -> error
     end
-  end
-
-  @spec escape_leading_dot(iodata) :: iodata
-  defp escape_leading_dot(message) do
-    message = IO.iodata_to_binary(message)
-    escape_leading_dot(message, 0, 0, message, [])
-  end
-
-  defp escape_leading_dot(<<?\n, ?., rest::bytes>>, from, len, original, acc) do
-    acc = ["\n..", binary_part(original, from, len) | acc]
-    escape_leading_dot(rest, from + len + 2, 0, original, acc)
-  end
-
-  defp escape_leading_dot(<<_, rest::bytes>>, from, len, original, acc) do
-    escape_leading_dot(rest, from, len + 1, original, acc)
-  end
-
-  defp escape_leading_dot(<<>>, _from, _len, original, []) do
-    original
-  end
-
-  defp escape_leading_dot(<<>>, from, len, original, acc) do
-    escaped_parts = [binary_part(original, from, len) | acc]
-    :lists.reverse(escaped_parts)
   end
 
   @doc """
