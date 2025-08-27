8b101e7b6ea7d9a29cccd683e2076925cec0e2fd
ruslandoga <ruslandoga+gh@icloud.com>
2025-08-27 15:50:54 +0900

    back to 1.13

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Mua.MixProject do
     [
       app: :mua,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
