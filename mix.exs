e36f5362a75f42a57ed2aec0f44f337b26f9a7e7
ruslandoga <ruslandoga+gh@icloud.com>
2025-08-27 15:49:35 +0900

    require 1.14

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Mua.MixProject do
     [
       app: :mua,
       version: @version,
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
